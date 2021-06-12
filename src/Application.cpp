7935b302ca0daba3e3959981fd1bd0cb91abce3d
Hochheilige <hochheilige01@gmail.com>
2021-06-13 00:02:35 +0300

    finally rotating cube

--- src/Application.cpp
+++ src/Application.cpp
@@ -594,6 +594,140 @@ int main(int argc, char* argv[]) {
 
 	device.bindBufferMemory(vertexBuffer, deviceMemory, 0);
 
+	// pipeline 
+	std::array<vk::PipelineShaderStageCreateInfo, 2> pipelineShaderStagesCreateInfos = {
+		vk::PipelineShaderStageCreateInfo(
+			vk::PipelineShaderStageCreateFlags(),
+			vk::ShaderStageFlagBits::eVertex,
+			cubeVertexShaderModule,
+			"main"
+		),
+		vk::PipelineShaderStageCreateInfo(
+			vk::PipelineShaderStageCreateFlags(),
+			vk::ShaderStageFlagBits::eFragment,
+			cubeFragmentShaderModule,
+			"main"
+		)
+	};
+
+	vk::VertexInputBindingDescription vertexInputBindingDescription(
+		0,
+		sizeof(coloredCubeData[0])
+	);
+
+	std::array<vk::VertexInputAttributeDescription, 2> vertexInputAttributeDescriptions = {
+		vk::VertexInputAttributeDescription(0, 0, vk::Format::eR32G32B32A32Sfloat, 0),
+		vk::VertexInputAttributeDescription(1, 0, vk::Format::eR32G32B32A32Sfloat, 16)
+	};
+
+	vk::PipelineVertexInputStateCreateInfo pipelineVertexInputStateCreateInfo(
+		vk::PipelineVertexInputStateCreateFlags(),  // flags
+		vertexInputBindingDescription,              // vertexBindingDescriptions
+		vertexInputAttributeDescriptions            // vertexAttributeDescriptions
+	);
+
+	vk::PipelineInputAssemblyStateCreateInfo pipelineInputAssemblyCreateInfo(
+		vk::PipelineInputAssemblyStateCreateFlags(),
+		vk::PrimitiveTopology::eTriangleList
+	);
+
+	vk::PipelineViewportStateCreateInfo pipelineViewportStateCreateInfo(
+		vk::PipelineViewportStateCreateFlags(),
+		1,
+		nullptr,
+		1,
+		nullptr
+	);
+
+	vk::PipelineRasterizationStateCreateInfo pipelineRasterizationStateCreateInfo(
+		vk::PipelineRasterizationStateCreateFlags(),  // flags
+		false,                                        // depthClampEnable
+		false,                                        // rasterizerDiscardEnable
+		vk::PolygonMode::eFill,                       // polygonMode
+		vk::CullModeFlagBits::eBack,                  // cullMode
+		vk::FrontFace::eClockwise,                    // frontFace
+		false,                                        // depthBiasEnable
+		0.0f,                                         // depthBiasConstantFactor
+		0.0f,                                         // depthBiasClamp
+		0.0f,                                         // depthBiasSlopeFactor
+		1.0f                                          // lineWidth
+	);
+
+	vk::PipelineMultisampleStateCreateInfo pipelineMultisampleStateCreateInfo(
+		vk::PipelineMultisampleStateCreateFlags(),
+		vk::SampleCountFlagBits::e1
+	);
+
+	vk::StencilOpState stencilOpState(
+		vk::StencilOp::eKeep,
+		vk::StencilOp::eKeep,
+		vk::StencilOp::eKeep,
+		vk::CompareOp::eAlways
+	);
+
+	vk::PipelineDepthStencilStateCreateInfo pipelineDepthStencilStateCreateInfo(
+		vk::PipelineDepthStencilStateCreateFlags(),  // flags
+		true,                                        // depthTestEnable
+		true,                                        // depthWriteEnable
+		vk::CompareOp::eLessOrEqual,                 // depthCompareOp
+		false,                                       // depthBoundTestEnable
+		false,                                       // stencilTestEnable
+		stencilOpState,                              // front
+		stencilOpState                               // back
+	);
+
+	vk::ColorComponentFlags colorComponentFlags(
+		vk::ColorComponentFlagBits::eR | vk::ColorComponentFlagBits::eG |
+		vk::ColorComponentFlagBits::eB | vk::ColorComponentFlagBits::eA
+	);
+
+	vk::PipelineColorBlendAttachmentState pipelineColorBlendAttachmentState(
+		false,                   // blendEnable
+		vk::BlendFactor::eZero,  // srcColorBlendFactor
+		vk::BlendFactor::eZero,  // dstColorBlendFactor
+		vk::BlendOp::eAdd,       // colorBlendOp
+		vk::BlendFactor::eZero,  // srcAlphaBlendFactor
+		vk::BlendFactor::eZero,  // dstAlphaBlendFactor
+		vk::BlendOp::eAdd,       // alphaBlendOp
+		colorComponentFlags      // colorWriteMask
+	);
+
+	vk::PipelineColorBlendStateCreateInfo pipelineColorBlendStateCreateInfo(
+		vk::PipelineColorBlendStateCreateFlags(),  // flags
+		false,                                     // logicOpEnable
+		vk::LogicOp::eNoOp,                        // logicOp
+		pipelineColorBlendAttachmentState,         // attachments
+		{ { 1.0f, 1.0f, 1.0f, 1.0f } }             // blendConstants
+	);
+
+	std::array<vk::DynamicState, 2> dynamicStates = { vk::DynamicState::eViewport, vk::DynamicState::eScissor };
+	vk::PipelineDynamicStateCreateInfo pipelineDynamicStateCreateInfo(
+		vk::PipelineDynamicStateCreateFlags(),
+		dynamicStates
+	);
+
+	vk::GraphicsPipelineCreateInfo graphicsPipelineCreateInfo(
+		vk::PipelineCreateFlags(),
+		pipelineShaderStagesCreateInfos,
+		&pipelineVertexInputStateCreateInfo,
+		&pipelineInputAssemblyCreateInfo,
+		nullptr,
+		&pipelineViewportStateCreateInfo,
+		&pipelineRasterizationStateCreateInfo,
+		&pipelineMultisampleStateCreateInfo,
+		&pipelineDepthStencilStateCreateInfo,
+		&pipelineColorBlendStateCreateInfo,
+		&pipelineDynamicStateCreateInfo,
+		pipelineLayout,
+		renderPass
+	);
+
+	vk::Result result;
+	vk::Pipeline pipeline;
+	std::tie(result, pipeline) = device.createGraphicsPipeline(nullptr, graphicsPipelineCreateInfo);
+
+
+	uint32_t frameNumber = 0;
 	while (!isShouldClose) {
 		while (SDL_PollEvent(&event) != 0) {
 			if (event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE))
@@ -622,6 +756,40 @@ int main(int argc, char* argv[]) {
 		clearValues[0].color = vk::ClearColorValue(std::array<float, 4>({ {redFlash, greenFlash, blueFlash, alphaFlash} }));
 		clearValues[1].depthStencil = vk::ClearDepthStencilValue(1.0f, 0);
 
+		// Uniform buffer data
+		model = glm::rotate(glm::mat4{ 1.0f }, glm::radians(frameNumber * 0.7f), glm::vec3(1, 0.5, 1));
+		view = glm::lookAt(
+			glm::vec3(-5.0f, 3.0f, -10.0f),
+			glm::vec3(0.0f, 0.0f, 0.0f),
+			glm::vec3(0.0f, -1.0f, 0.0f)
+		);
+		projection = glm::perspective(glm::radians(45.0f), 1.0f, 0.1f, 100.0f);
+		clip = glm::mat4x4(
+			1.0f, 0.0f, 0.0f, 0.0f,
+			0.0f, -1.0f, 0.0f, 0.0f,
+			0.0f, 0.0f, 0.5f, 0.0f,
+			0.0f, 0.0f, 0.5f, 1.0f
+		);
+		mvpc = clip * projection * view * model;
+		++frameNumber;
+
+		//vk::Buffer stagingDataBuffer = device.createBuffer(
+		//	vk::BufferCreateInfo(vk::BufferCreateFlags(), sizeof(mvpc), vk::BufferUsageFlagBits::eTransferSrc)
+		//);
+
+		//vk::MemoryRequirements uniformMemoryReq = device.getBufferMemoryRequirements(uniformDataBuffer);
+		//uint32_t uniformTypeIndex = findMemoryType(
+		//	memoryProperties,
+		//	uniformMemoryReq.memoryTypeBits,
+		//	vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent
+		//);
+
+		//vk::DeviceMemory uniformDataMemory = device.allocateMemory(vk::MemoryAllocateInfo(uniformMemoryReq.size, uniformTypeIndex));
+
+		data = static_cast<uint8_t*>(device.mapMemory(uniformDataMemory, 0, uniformMemoryReq.size));
+		memcpy(data, &mvpc, sizeof(mvpc));
+		//device.unmapMemory(uniformDataMemory);
+
 		commandBuffer.begin(vk::CommandBufferBeginInfo(vk::CommandBufferUsageFlags()));
 
 		vk::RenderPassBeginInfo renderPassBeginInfo(
@@ -633,17 +801,57 @@ int main(int argc, char* argv[]) {
 
 		commandBuffer.beginRenderPass(renderPassBeginInfo, vk::SubpassContents::eInline);
 
+		commandBuffer.bindPipeline(vk::PipelineBindPoint::eGraphics, pipeline);
+		// ???
+		commandBuffer.bindDescriptorSets(vk::PipelineBindPoint::eGraphics, pipelineLayout, 0, descriptorSet, nullptr);
 		
+		//commandBuffer.copyBuffer();
+
 		commandBuffer.bindVertexBuffers(0, vertexBuffer, { 0 });
+
+		commandBuffer.setViewport(
+			0,
+			vk::Viewport(
+				0.0f, 0.0f,
+				static_cast<float>(swapchainExtent.width),
+				static_cast<float>(swapchainExtent.height),
+				0.0f,
+				1.0f
+			)
+		);
+
+		commandBuffer.setScissor(0, vk::Rect2D(vk::Offset2D(0, 0), swapchainExtent));
+
+		commandBuffer.draw(12 * 3, 1, 0, 0);
 
 		commandBuffer.endRenderPass();
 		commandBuffer.end();
 
-		submitAndWait(device, graphicsQueue, commandBuffer);
-		graphicsQueue.presentKHR(vk::PresentInfoKHR({}, swapchain, currentBuffer.value));
+		// rewrite
+		//submitAndWait(device, graphicsQueue, commandBuffer);
+		
+		// submiting to queue and present
+		vk::Fence drawFence = device.createFence(vk::FenceCreateInfo());
+		vk::PipelineStageFlags waitDestinationStageMask(vk::PipelineStageFlagBits::eColorAttachmentOutput);
+		vk::SubmitInfo submitInfo(
+			imageAcquiredSemaphore,
+			waitDestinationStageMask,
+			commandBuffer
+		);
+		graphicsQueue.submit(submitInfo, drawFence);
+
+		while (vk::Result::eTimeout == device.waitForFences(drawFence, VK_TRUE, 1000000000));
+
+		vk::Result drawResult = graphicsQueue.presentKHR(vk::PresentInfoKHR({}, swapchain, currentBuffer.value));
+
+		device.waitIdle();
+
+		device.destroyFence(drawFence);
 		device.destroySemaphore(imageAcquiredSemaphore);
 	}
 	
+	device.destroyPipeline(pipeline);
+
 	device.freeMemory(deviceMemory);
 	device.destroyBuffer(vertexBuffer);
 
